1c2f4d017e358da34fb35511187cf310b7c32cb0
Maunder <maunderscott@gmail.com>
2025-04-10 23:36:04 +1200

    Update doc blocks

--- src/operators/spatial-operators.ts
+++ src/operators/spatial-operators.ts
@@ -5,7 +5,15 @@ import type { Condition } from "./base-types";
  * Creates a spatial INTERSECTS condition
  * @example
  * intersects("geometry", { type: "Point", coordinates: [0, 0] })
- * @see {@link https://docs.ogc.org/is/21-065r2/21-065r2.html OGC CQL - Spatial Operators}
+ *  generates: INTERSECTS(geometry, POINT(0 0))
+ *  Other outputs based on geometry type:
+ *  - INTERSECTS(geometry, POINT(0 0))
+ *  - INTERSECTS(geometry, LINESTRING(0 0, 1 1, 2 2))
+ *  - INTERSECTS(geometry, POLYGON((0 0, 1 0, 1 1, 0 1, 0 0)))
+ *  - INTERSECTS(geometry, MULTIPOINT((0 0), (1 1)))
+ *  - INTERSECTS(geometry, MULTILINESTRING((0 0, 1 1), (2 2, 3 3)))
+ *  - INTERSECTS(geometry, MULTIPOLYGON(((0 0, 1 0, 1 1, 0 1, 0 0)), ((2 2, 3 2, 3 3, 2 3, 2 2))))
+ * @see {@link https:docs.ogc.org/is/21-065r2/21-065r2.html OGC CQL - Spatial Operators}
  */
 export const intersects = (attr: string, geometry: Geometry): Condition => ({
   type: "intersects",
@@ -17,7 +25,15 @@ export const intersects = (attr: string, geometry: Geometry): Condition => ({
  * Creates a spatial DISJOINT condition
  * @example
  * disjoint("geometry", { type: "Point", coordinates: [0, 0] })
- * @see {@link https://docs.ogc.org/is/21-065r2/21-065r2.html OGC CQL - Spatial Operators}
+ *  generates: DISJOINT(geometry, POINT(0 0))
+ *  Other outputs based on geometry type:
+ *  - DISJOINT(geometry, POINT(0 0))
+ *  - DISJOINT(geometry, LINESTRING(0 0, 1 1, 2 2))
+ *  - DISJOINT(geometry, POLYGON((0 0, 1 0, 1 1, 0 1, 0 0)))
+ *  - DISJOINT(geometry, MULTIPOINT((0 0), (1 1)))
+ *  - DISJOINT(geometry, MULTILINESTRING((0 0, 1 1), (2 2, 3 3)))
+ *  - DISJOINT(geometry, MULTIPOLYGON(((0 0, 1 0, 1 1, 0 1, 0 0)), ((2 2, 3 2, 3 3, 2 3, 2 2))))
+ * @see {@link https:docs.ogc.org/is/21-065r2/21-065r2.html OGC CQL - Spatial Operators}
  */
 export const disjoint = (attr: string, geometry: Geometry): Condition => ({
   type: "disjoint",
@@ -29,7 +45,15 @@ export const disjoint = (attr: string, geometry: Geometry): Condition => ({
  * Creates a spatial CONTAINS condition
  * @example
  * spatialContains("geometry", { type: "Point", coordinates: [0, 0] })
- * @see {@link https://docs.ogc.org/is/21-065r2/21-065r2.html OGC CQL - Spatial Operators}
+ *  generates: CONTAINS(geometry, POINT(0 0))
+ *  Other outputs based on geometry type:
+ *  - CONTAINS(geometry, POINT(0 0))
+ *  - CONTAINS(geometry, LINESTRING(0 0, 1 1, 2 2))
+ *  - CONTAINS(geometry, POLYGON((0 0, 1 0, 1 1, 0 1, 0 0)))
+ *  - CONTAINS(geometry, MULTIPOINT((0 0), (1 1)))
+ *  - CONTAINS(geometry, MULTILINESTRING((0 0, 1 1), (2 2, 3 3)))
+ *  - CONTAINS(geometry, MULTIPOLYGON(((0 0, 1 0, 1 1, 0 1, 0 0)), ((2 2, 3 2, 3 3, 2 3, 2 2))))
+ * @see {@link https:docs.ogc.org/is/21-065r2/21-065r2.html OGC CQL - Spatial Operators}
  */
 export const spatialContains = (
   attr: string,
@@ -44,7 +68,15 @@ export const spatialContains = (
  * Creates a spatial WITHIN condition
  * @example
  * within("geometry", { type: "Polygon", coordinates: [[[0, 0], [1, 0], [1, 1], [0, 1], [0, 0]]] })
- * @see {@link https://docs.ogc.org/is/21-065r2/21-065r2.html OGC CQL - Spatial Operators}
+ *  generates: WITHIN(geometry, POLYGON((0 0, 1 0, 1 1, 0 1, 0 0)))
+ *  Other outputs based on geometry type:
+ *  - WITHIN(geometry, POINT(0 0))
+ *  - WITHIN(geometry, LINESTRING(0 0, 1 1, 2 2))
+ *  - WITHIN(geometry, POLYGON((0 0, 1 0, 1 1, 0 1, 0 0)))
+ *  - WITHIN(geometry, MULTIPOINT((0 0), (1 1)))
+ *  - WITHIN(geometry, MULTILINESTRING((0 0, 1 1), (2 2, 3 3)))
+ *  - WITHIN(geometry, MULTIPOLYGON(((0 0, 1 0, 1 1, 0 1, 0 0)), ((2 2, 3 2, 3 3, 2 3, 2 2))))
+ * @see {@link https:docs.ogc.org/is/21-065r2/21-065r2.html OGC CQL - Spatial Operators}
  */
 export const within = (attr: string, geometry: Geometry): Condition => ({
   type: "within",
@@ -56,7 +88,15 @@ export const within = (attr: string, geometry: Geometry): Condition => ({
  * Creates a spatial TOUCHES condition
  * @example
  * touches("geometry", { type: "LineString", coordinates: [[0, 0], [1, 1]] })
- * @see {@link https://docs.ogc.org/is/21-065r2/21-065r2.html OGC CQL - Spatial Operators}
+ *  generates: TOUCHES(geometry, LINESTRING(0 0, 1 1))
+ *  Other outputs based on geometry type:
+ *  - TOUCHES(geometry, POINT(0 0))
+ *  - TOUCHES(geometry, LINESTRING(0 0, 1 1, 2 2))
+ *  - TOUCHES(geometry, POLYGON((0 0, 1 0, 1 1, 0 1, 0 0)))
+ *  - TOUCHES(geometry, MULTIPOINT((0 0), (1 1)))
+ *  - TOUCHES(geometry, MULTILINESTRING((0 0, 1 1), (2 2, 3 3)))
+ *  - TOUCHES(geometry, MULTIPOLYGON(((0 0, 1 0, 1 1, 0 1, 0 0)), ((2 2, 3 2, 3 3, 2 3, 2 2))))
+ * @see {@link https:docs.ogc.org/is/21-065r2/21-065r2.html OGC CQL - Spatial Operators}
  */
 export const touches = (attr: string, geometry: Geometry): Condition => ({
   type: "touches",
@@ -68,7 +108,15 @@ export const touches = (attr: string, geometry: Geometry): Condition => ({
  * Creates a spatial OVERLAPS condition
  * @example
  * overlaps("geometry", { type: "Polygon", coordinates: [[[0, 0], [1, 0], [1, 1], [0, 1], [0, 0]]] })
- * @see {@link https://docs.ogc.org/is/21-065r2/21-065r2.html OGC CQL - Spatial Operators}
+ *  generates: OVERLAPS(geometry, POLYGON((0 0, 1 0, 1 1, 0 1, 0 0)))
+ *  Other outputs based on geometry type:
+ *  - OVERLAPS(geometry, POINT(0 0))
+ *  - OVERLAPS(geometry, LINESTRING(0 0, 1 1, 2 2))
+ *  - OVERLAPS(geometry, POLYGON((0 0, 1 0, 1 1, 0 1, 0 0)))
+ *  - OVERLAPS(geometry, MULTIPOINT((0 0), (1 1)))
+ *  - OVERLAPS(geometry, MULTILINESTRING((0 0, 1 1), (2 2, 3 3)))
+ *  - OVERLAPS(geometry, MULTIPOLYGON(((0 0, 1 0, 1 1, 0 1, 0 0)), ((2 2, 3 2, 3 3, 2 3, 2 2))))
+ * @see {@link https:docs.ogc.org/is/21-065r2/21-065r2.html OGC CQL - Spatial Operators}
  */
 export const overlaps = (attr: string, geometry: Geometry): Condition => ({
   type: "overlaps",
@@ -80,7 +128,15 @@ export const overlaps = (attr: string, geometry: Geometry): Condition => ({
  * Creates a spatial CROSSES condition
  * @example
  * crosses("geometry", { type: "LineString", coordinates: [[0, 0], [1, 1]] })
- * @see {@link https://docs.ogc.org/is/21-065r2/21-065r2.html OGC CQL - Spatial Operators}
+ *  generates: CROSSES(geometry, LINESTRING(0 0, 1 1))
+ *  Other outputs based on geometry type:
+ *  - CROSSES(geometry, POINT(0 0))
+ *  - CROSSES(geometry, LINESTRING(0 0, 1 1, 2 2))
+ *  - CROSSES(geometry, POLYGON((0 0, 1 0, 1 1, 0 1, 0 0)))
+ *  - CROSSES(geometry, MULTIPOINT((0 0), (1 1)))
+ *  - CROSSES(geometry, MULTILINESTRING((0 0, 1 1), (2 2, 3 3)))
+ *  - CROSSES(geometry, MULTIPOLYGON(((0 0, 1 0, 1 1, 0 1, 0 0)), ((2 2, 3 2, 3 3, 2 3, 2 2))))
+ * @see {@link https:docs.ogc.org/is/21-065r2/21-065r2.html OGC CQL - Spatial Operators}
  */
 export const crosses = (attr: string, geometry: Geometry): Condition => ({
   type: "crosses",
@@ -92,7 +148,15 @@ export const crosses = (attr: string, geometry: Geometry): Condition => ({
  * Creates a spatial EQUALS condition
  * @example
  * spatialEquals("geometry", { type: "Point", coordinates: [0, 0] })
- * @see {@link https://docs.ogc.org/is/21-065r2/21-065r2.html OGC CQL - Spatial Operators}
+ *  generates: EQUALS(geometry, POINT(0 0))
+ *  Other outputs based on geometry type:
+ *  - EQUALS(geometry, POINT(0 0))
+ *  - EQUALS(geometry, LINESTRING(0 0, 1 1, 2 2))
+ *  - EQUALS(geometry, POLYGON((0 0, 1 0, 1 1, 0 1, 0 0)))
+ *  - EQUALS(geometry, MULTIPOINT((0 0), (1 1)))
+ *  - EQUALS(geometry, MULTILINESTRING((0 0, 1 1), (2 2, 3 3)))
+ *  - EQUALS(geometry, MULTIPOLYGON(((0 0, 1 0, 1 1, 0 1, 0 0)), ((2 2, 3 2, 3 3, 2 3, 2 2))))
+ * @see {@link https:docs.ogc.org/is/21-065r2/21-065r2.html OGC CQL - Spatial Operators}
  */
 export const spatialEquals = (attr: string, geometry: Geometry): Condition => ({
   type: "eq",

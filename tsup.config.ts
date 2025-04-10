ef1f2c4fb63e58233448da5ee168c72227e7b0ba
Maunder <maunderscott@gmail.com>
2025-04-10 23:46:37 +1200

    Fix build

--- tsup.config.ts
+++ tsup.config.ts
@@ -5,10 +5,11 @@ export default defineConfig({
   dts: true,
   outDir: "dist",
   entry: {
-    "operators/comparison": "src/operators/comparison.ts",
-    "operators/logical": "src/operators/logical.ts",
-    "operators/spatial": "src/operators/spatial.ts",
-    "operators/text": "src/operators/text.ts",
+    "operators/comparison": "src/operators/comparison-operators.ts",
+    "operators/logical": "src/operators/logical-operators.ts",
+    "operators/spatial": "src/operators/spatial-operators.ts",
+    "operators/text": "src/operators/text-operators.ts",
     "query-builder": "src/query-builder.ts",
+    errors: "src/errors.ts",
   },
 });
